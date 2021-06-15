b77816bb775030f3727a5266bf0d6353cbacc86a
douglas86 <douglasmaxton@gmail.com>
2021-06-15 13:27:38 +0200

    Create try and catch for createPost for db

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,6 +9,13 @@ export const getPosts = async (req, res) => {
     }
 };
 
-export const createPost = (req, res) => {
-    res.send('Post creation');
+export const createPost = async (req, res) => {
+    const body = req.body;
+    const newPost = new PostMessage(body);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 };
