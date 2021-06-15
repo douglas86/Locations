ffd0d2c3fa77bc79579038d67492983e98dc818e
douglas86 <douglasmaxton@gmail.com>
2021-06-15 13:11:34 +0200

    Created a new route for posts

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import keys from './config/keys';
 
+import postRoutes from './routes/posts';
+
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = keys.mongodb.dbURI;
 const PORT = process.env.PORT || 5000;
